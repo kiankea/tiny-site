937d1b7b7018ffa9a07c5acc5addc5c06018103a
kiankea <74528778+kiankea@users.noreply.github.com>
2020-11-16 13:24:04 +0100

    Uploaded files. Initial.

---
+++ tiny-site/src/test/java/kcn/kea/tinysite/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package kcn.kea.tinysite.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
