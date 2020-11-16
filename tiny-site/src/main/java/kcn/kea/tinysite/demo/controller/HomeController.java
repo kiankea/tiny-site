2ba43a6a8f3bef4bb95805c04644ba25c99a3168
kiankea <74528778+kiankea@users.noreply.github.com>
2020-11-16 17:30:54 +0100

    Update HomeController.java

--- tiny-site/src/main/java/kcn/kea/tinysite/demo/controller/HomeController.java
+++ tiny-site/src/main/java/kcn/kea/tinysite/demo/controller/HomeController.java
@@ -8,6 +8,7 @@ public class HomeController
 {
     @GetMapping("/")
     public String showHome(){
+        System.out.println("Accessing '/' mapping in HomeController");
         return "index";
     }
 
